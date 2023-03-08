7d87cd769de1141141daeeaf76ead4cf8930d1e9
Alejandro9998 <51530832+Alejandro9998@users.noreply.github.com>
2023-03-08 23:38:13 +0100

    Update tests.py

--- tests.py
+++ tests.py
@@ -4,27 +4,23 @@ from article_analysis import *
 class TestArticleAnalysis(unittest.TestCase):
 
     def test_text_extraction(self):
-        # Test text extraction for one article
         text = extract_text("article1.pdf")
         self.assertEqual(type(text), str)
         self.assertNotEqual(len(text), 0)
         
     def test_keyword_extraction(self):
-        # Test keyword extraction for one article
         text = extract_text("article1.pdf")
         keywords = extract_keywords(text)
         self.assertEqual(type(keywords), list)
         self.assertNotEqual(len(keywords), 0)
         
     def test_figure_count(self):
-        # Test figure count for one article
         text = extract_text("article1.pdf")
         figure_count = count_figures(text)
         self.assertEqual(type(figure_count), int)
         self.assertGreaterEqual(figure_count, 0)
         
     def test_link_extraction(self):
-        # Test link extraction for one article
         links = extract_links("article1.pdf")
         self.assertEqual(type(links), list)
         self.assertNotEqual(len(links), 0)
